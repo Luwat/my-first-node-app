817b351085f509e32f4ee2ebcfe406ce546ace14
Oluwatobi <oluwatobioreniyi@gmail.com>
2025-07-21 12:53:43 +0100

    Implement dynamic routing for HTML files in the Node.js server

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 const http = require('node:http');
+const path = require('node:path');
+const fs = require('node:fs');
 
 const port = 8080;
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('<h1>Hello, World!</h1><p>This is my first Node.js application.</p>');
+    let filePath = '';
+    if (req.url === '/' || req.url === '/index.html') {
+        filePath = path.join(__dirname, 'index.html');
+    } else if (req.url === '/about.html') {
+        filePath = path.join(__dirname, 'about.html');
+    } else if (req.url === '/contact.html') {
+        filePath = path.join(__dirname, 'contact.html');
+    } else {
+        filePath = path.join(__dirname, '404.html');
+    }
+
+    fs.readFile(filePath, (error, content) => {
+        if (error) {
+            res.writeHead(500, { 'Content-Type': 'text/plain' });
+            res.end('Server Error');
+        } else {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(content);
+        }
+    })
 })
 
 server.listen(port, () => {
